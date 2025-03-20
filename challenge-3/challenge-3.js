34c7961c0755834baf44e73aa6cb9112adba3572
Elias El Khoury <eliaskhoury234@gmail.com>
2025-03-20 18:02:31 +0200

    Added challenge-3 solution for assessment-1

---
+++ challenge-3/challenge-3.js
@@ -0,0 +1,9 @@
+function findMissingNumber(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i] !== i + 1) {
+            return i + 1; // The missing number
+        }
+    }
+    return -1; // Return -1 if no missing number is found
+}
+console.log(findMissingNumber([1, 2, 4, 5, 6]));
